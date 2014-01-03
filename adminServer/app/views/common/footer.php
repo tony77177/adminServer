edfb1f270acd91b50f3a150c7ba5ed74d91833ed
tony77177 <tony77177@gmail.com>
2014-01-03 01:23:38 -0800

    页脚修改为自动获取当前年份

--- adminServer/app/views/common/footer.php
+++ adminServer/app/views/common/footer.php
@@ -1,7 +1,7 @@
 <hr/>
 <footer>
     <div class="text-center muted">
-        &copy; 2013-2014
+        &copy; <script>document.write((new Date().getFullYear()))</script>
         后台管理系统 <br/>
         Powered by <a href="http://www.freedomdream.cn" target="_blank">畅想科技</a>
     </div>
